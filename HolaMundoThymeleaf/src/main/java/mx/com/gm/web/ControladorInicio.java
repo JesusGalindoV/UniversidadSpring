f6e5206465748f7b62711eecdb9681ae6b2048ab
Jesus Galindo <janthonygalindov@hotmail.com>
2022-07-02 15:31:23 -0700

    CRUD completo

--- HolaMundoThymeleaf/src/main/java/mx/com/gm/web/ControladorInicio.java
+++ HolaMundoThymeleaf/src/main/java/mx/com/gm/web/ControladorInicio.java
@@ -2,18 +2,19 @@ package mx.com.gm.web;
 
 import lombok.extern.slf4j.Slf4j;
 
+import mx.com.gm.domain.Persona;
 import mx.com.gm.services.PersonaService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 @Slf4j
 public class ControladorInicio {
-
     @Autowired
     private PersonaService personaService;
 
@@ -21,10 +22,44 @@ public class ControladorInicio {
     public String inicio(Model model){
 
         var personas = personaService.listarPersonas();
-        log.info("Ejecutando el controlador Spring MVC - database connection.");
+        log.info("Ejecutando el controlador Spring MVC - database connection. 05");
 
         model.addAttribute("personas", personas);
 
         return "index";
     }
+
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+
+        return "modificar";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+
+        personaService.guardar(persona);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model){
+
+        persona = personaService.encontrarPersona(persona);
+
+        model.addAttribute("persona", persona);
+
+        return "modificar";
+    }
+
+    @GetMapping("/eliminar")
+    public String eliminar(Persona persona, Model model){
+
+        personaService.eliminar(persona);
+
+        return "redirect:/";
+
+    }
+
 }
